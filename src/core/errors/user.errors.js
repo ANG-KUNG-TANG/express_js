5a8b9e6dbb2115672de422f3d0d7e41305d37b43
AnriAngKung <anrikung2002@gmail.com>
2026-05-03 21:45:41 +0700

    update: modify user error add usernamerequire

--- src/core/errors/user.errors.js
+++ src/core/errors/user.errors.js
@@ -83,6 +83,7 @@ export class UserInvalidRoleError extends UserValidationError {
     }
 }
 
+
 // ------------------------------------------------------------------
 // Profile Validation
 // ------------------------------------------------------------------
